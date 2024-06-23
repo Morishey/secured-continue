54cd260938d19230b2c5e52b88acd41a1bee2552
Morishey <morishey@onmail.com>
2024-06-23 06:33:57 -0600

    php modified

--- submit.php
+++ submit.php
@@ -1,9 +1,14 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    // Collect and sanitize form data
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
+    if (!$email) {
+        echo "Invalid email format.";
+        exit;
+    }
 
     // Email details
     $to = "jaeger.62frei@gmail.com";
