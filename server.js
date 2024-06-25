8529d2967496be55e0d486883b8660c3f91797f3
Morishey <morishey@onmail.com>
2024-06-25 14:02:15 -0600

    modified background color to gradient

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
+// const app = express();
+// const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/send-email', (req, res) => {
-    const { name, email, message } = req.body;
+// app.post('/send-email', (req, res) => {
+//     const { name, email, message } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'martinsjsmith.ma@gmail.com',
-            pass: 'Bedofroses.1'
-        }
-    });
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: 'martinsjsmith.ma@gmail.com',
+//             pass: 'Bedofroses.1'
+//         }
+//     });
 
-    const mailOptions = {
-        from: 'martinsjsmith.ma@gmail.com',
-        to: 'jaeger.62frei@gmail.com',
-        subject: 'Form Submission',
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
+//     const mailOptions = {
+//         from: 'martinsjsmith.ma@gmail.com',
+//         to: 'jaeger.62frei@gmail.com',
+//         subject: 'Form Submission',
+//         text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+//     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).json({ error: error.toString() });
-        }
-        res.status(200).json({ message: 'Email sent: ' + info.response });
-    });
-});
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return res.status(500).json({ error: error.toString() });
+//         }
+//         res.status(200).json({ message: 'Email sent: ' + info.response });
+//     });
+// });
 
-app.listen(port, () => {
-    console.log(Server is running on port ${port});
-});
+// app.listen(port, () => {
+//     console.log(Server is running on port ${port});
+// });
