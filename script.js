ec16fa203c8a06ece308361a9ce55bc623f34a1b
Morishey <morishey@onmail.com>
2024-07-13 08:43:24 -0600

    confetti modified

--- script.js
+++ script.js
@@ -36,48 +36,19 @@ document.querySelector('.login-btn').addEventListener('click', function () {
 
 
 
-
-// HANDLE FORM SUBMISSION
-// document.getElementById('login-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     const formData = new FormData(this);
-//     const data = {};
-//     formData.forEach((value, key) => {
-//         data[key] = value;
-//     });
-
-//     fetch('http://your-server-url/send-email', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     })
-//     .then(response => response.json())
-//     .then(result => {
-//         alert('Form submitted successfully!');
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
-// });
-
-
-
-// RIBBONS
+// CONFETTI
 
 document.addEventListener("DOMContentLoaded", () => {
     
     confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: {x: 1, y: 0.9 },
+        particleCount: 120,
+        spread: 100,
+        origin: {x: 1, y: 0.7 },
     });
     confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: {x: 0, y: 0.9 },
+        particleCount: 120,
+        spread: 100,
+        origin: {x: 0, y: 0.7 },
     });
 
 });
